b5e09a6b5b8990431d0420fc71b68dac2e8f5ceb
vilar <loadslowly@gmail.com>
2023-11-26 12:58:57 +0300

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import './App.css'
 import {useEffect} from "react";
 import MyHeader from "./components/Header/MyHeader.tsx";
 import {useTelegram} from "./hooks/useTelegram.ts";
+import {Route, Routes} from "react-router-dom";
+import ProductList from "./components/ProductList/ProductList.tsx";
+import Form from "./components/Form/Form.tsx";
+
 
 function App() {
-    const {tg, onToggleButton} = useTelegram();
+    const {tg} = useTelegram();
 
     useEffect(() => {
         tg.ready();
@@ -13,8 +17,10 @@ function App() {
     return (
         <div>
             <MyHeader />
-            Work
-            <button onClick={onToggleButton}>toggle</button>
+            <Routes>
+                <Route index element={<ProductList/>} />
+                <Route path={'/form'} element={<Form/>} />
+            </Routes>
         </div>
     )
 }
---
+++ src/components/ProductList/ProductList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ProductList.css'
+
+const ProductList = () => {
+    return (
+        <div>
+            PRODUCT LIST
+        </div>
+    );
+};
+
+export default ProductList;
